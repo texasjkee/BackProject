86cd878f9e735c4459c77bd7cece487c1cb78311
texasjkee <gt.gluck@gmail.com>
2023-01-25 19:47:50 +0200

    Add try catch to post method

--- index.js
+++ index.js
@@ -30,23 +30,46 @@ app.listen(PORT, (err) => {
 })
 
 app.post('/auth/register', registerValidatior, async (req, res) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+  
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel ({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash 
+    })
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret333',
+      {
+        expiresIn: '30d'
+      }
+    )
+
+    const {passwordHash, ...userData} = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+      
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: "Failed to register"
+    })
   }
- 
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel ({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.passwordHash,
-  })
-
-  res.json({
-    success: true,
-  })
 })
