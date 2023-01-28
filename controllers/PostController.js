31b17ff93e8625049bc1104c77fc8185dd3719dc
texasjkee <gt.gluck@gmail.com>
2023-01-28 14:05:32 +0200

    Create update method

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -91,7 +91,7 @@ export const create = async (req, res) => {
   try {
     const doc = new PostModel({
       title: req.body.title,
-      text: req.body.title,
+      text: req.body.text,
       imageUrl: req.body.imageUrl,
       tags: req.body.tags,
       user: req.userId,
@@ -105,5 +105,30 @@ export const create = async (req, res) => {
     res.status(500).json({
       message: 'Cant create title'
     })  
+  }
+}
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id; 
+    
+    await PostModel.updateOne({
+      _id: postId,
+    }, {
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    res.json({
+      success: true,
+    })
+  } catch (error) {
+    console.log(error) 
+    res.status(500).json({
+      message: 'Cant update the title'
+    })
   }
 }
